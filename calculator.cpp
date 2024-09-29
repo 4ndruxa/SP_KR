7c7cc0f7146fd8dd1dbfb26ec0d978742740d76d
Andrii <bodnarandrew123@gmail.com>
2024-09-29 17:57:48 +0300

    Added division function

--- calculator.cpp
+++ calculator.cpp
@@ -9,6 +9,13 @@ int Calculator::Add (double a, double b)
 
 }
 
+int Calculator::Div (double a, double b) {
+    if (b == 0) {
+        throw std::invalid_argument("Division by zero is not allowed");
+    }
+    return a / b;
+}
+
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
